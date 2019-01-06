dc1ced0552c4c7bd03d96bf61292298caf13c888
Administrator <admin@example.com>
2019-01-06 15:50:30 +0800

    update readme

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import "leetcode/sort"
 
 func testSort() {
 	var array = []int{1,3,7,6,4,2,8,9,10,5}
-	sort.Bubble(array, len(array))
+	// sort.Bubble(array, len(array))
+	// fmt.Printf("%v", array)
+	sort.Qsort(array, len(array))
 	fmt.Printf("%v", array)
 }
 
 func main() {
 	// 排序算法
-	testSort()
+	// testSort()
+	var a = []int{1,2,3,4,5}
+	fmt.Printf("%v", a[:1])
+	fmt.Printf("%v", a[1:3])
 }
--- sort/Sort.go
+++ sort/Sort.go
@@ -1,5 +1,9 @@
 package sort
 
+import (
+	// "fmt"
+)
+
 /**
  * 冒泡排序
  */
@@ -24,12 +28,29 @@ func Qsort(array []int, length int)  {
 	if length <= 0 || array == nil {
 		return
 	}
-	for i := length -1; i > 0; i-- {
-		for j := 0; j < i; j++ {
-			// fmt.Printf("%d, %d\n", i, j)
-			if array[j] > array[i] {
-				array[i], array[j] = array[j], array[i]
-			}
+	// fmt.Printf("%v", array, length)
+	var i int = 0
+	var j int = length - 1
+	var p int = length / 2
+	// fmt.Printf("%v", i, j, p)
+
+	for i != j {
+		if array[i] < array[p] {
+			i++
+		} else {
+			array[i], array[p] = array[p], array[i]
+			p = i
+			// fmt.Printf("%d, %d\n", i, p)
 		}
+		if array[p] < array[j] {
+			j--
+		} else {
+			array[j], array[p] = array[p], array[j]
+			p = j
+		}
+		// fmt.Printf("%v", i, j ,p)
+		// fmt.Printf("%v", array)
 	}
+	Qsort(array[:p], p)
+	Qsort(array[p + 1:], length - p - 1)
 }
