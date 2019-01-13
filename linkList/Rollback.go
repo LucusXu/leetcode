8838de02e2124244a408d98e933a1fa0664f940e
Administrator <admin@example.com>
2019-01-13 20:41:33 +0800

    sort special linklist

--- linkList/Rollback.go
+++ linkList/Rollback.go
@@ -149,3 +149,101 @@ func Mid(head *LinkNode) *LinkNode {
 	}
 	return fast
 }
+
+// 排序单链表（奇数位升序，偶数位降序）
+func Sort(head *LinkNode) *LinkNode {
+	if head == nil {
+		return nil
+	}
+	// 1.先拆分链表，奇数一个，偶数一个
+	head1, head2 := Separate(head)
+	// 2.翻转降序链表
+	head2 = FullReverse(head2)
+	// 3.归并左右半个链表
+	head = MergeSort(head1, head2)
+	return head
+}
+
+func MergeSort(head1, head2 *LinkNode) *LinkNode {
+	if head1 == nil || head2 == nil {
+		return nil
+	}
+	p := head1
+	q := head2
+	head := new(LinkNode)
+	if p.value <= q.value {
+		head = head1
+		p = head1.next
+	} else {
+		head = head2
+		q = head2.next
+	}
+	x := head
+	for ; p != nil && q != nil; {
+		if p.value <= q.value {
+			x.next = p
+			x = p
+			p = p.next
+		} else {
+			x.next = q
+			x = q
+			q = q.next
+		}
+	}
+	if p != nil {
+		x.next = p
+	}
+	if q != nil {
+		x.next = q
+	}
+	return head
+}
+
+func Separate(head *LinkNode) (*LinkNode, *LinkNode) {
+	if head == nil {
+		return nil, nil
+	}
+	var i int = 1
+	head1 := head
+	head2 := head
+	p := head
+	p1 := head1
+	p2 := head2
+
+	for ;p != nil; {
+		// 奇数
+		if i % 2 == 1 {
+			p1.next = p
+		} else {
+			p2.next = p
+		}
+		i++
+		p = p.next
+	}
+	return head1, head2
+}
+
+// 翻转链表
+func FullReverse(head *LinkNode) *LinkNode {
+	if head == nil {
+		return nil
+	}
+	// 只有一个节点了
+	if head.next == nil {
+		return head
+	}
+
+	prev := new(LinkNode)
+	cur := head
+	p := head.next
+	for ; p.next != nil; {
+		cur.next = prev
+		prev = cur
+		cur = p
+	}
+	return cur
+}
+
+
+
+
