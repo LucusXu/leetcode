7c95252dcc9663345bc05b6833f0f6681200aa50
Administrator <admin@example.com>
2019-01-17 09:18:05 +0800

    add stack

---
+++ sort/Stack.go
@@ -0,0 +1,43 @@
+package sort
+
+import (
+	"github.com/cheekybits/genny/generic"
+	"sync"
+)
+
+type Item generic.Type
+
+type Stack struct {
+	// 栈中的数据
+	items []Item
+	// 锁
+	lock sync.RWMutex
+}
+
+/**
+ * 创建栈
+ */
+func (s *Stack)New(item Item) * Stack {
+	s.items = []Item{}
+	return s
+}
+
+/**
+ * 入栈
+ */
+func (s *Stack)Push(item Item) {
+	s.lock.Lock()
+	s.items = append(s.items, item)
+	s.lock.Unlock()
+}
+
+/**
+ * 出栈
+ */
+func (s *Stack)Pop() * Item {
+	s.lock.Lock()
+	item := s.items[len(s.items) - 1]
+	s.items = s.items[:len(s.items) - 1]
+	s.lock.Unlock()
+	return &item
+}
