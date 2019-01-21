45ceb59c9faac5daf348e5772f26605969a791db
徐立秋 <xuliqiu@dongqiudi.com>
2019-01-21 20:10:07 +0800

    add goroutine

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "leetcode/sort"
 import "leetcode/linkList"
 import (
 	"leetcode/string"
+	"sync/atomic"
 )
 
 // 排序算法
@@ -72,4 +73,23 @@ func main() {
 	// testRollback()
 	// testUniqCount()
 	// testMoreThanK()
+	var count uint32
+	trigger := func(i uint32, fn func()) {
+		for {
+			if n := atomic.LoadUint32(&count); n == i {
+				fn()
+				atomic.AddUint32(&count, 1)
+				break
+			}
+		}
+	}
+	for i := uint32(0); i < 10; i++ {
+		go func(i uint32) {
+			fn := func() {
+				fmt.Println(i)
+			}
+			trigger(i, fn)
+		}(i)
+	}
+	trigger(10, func() {})
 }
