40b9d50fd1ee3165faf25387a7a82b496e2020b7
徐立秋 <xuliqiu@dongqiudi.com>
2020-05-18 18:56:14 +0800

    update

--- ks.go
+++ ks.go
@@ -25,10 +25,45 @@ func longestPalind(str string) int {
 }
 
 
-func main() {
-	var str = "abccccdd"
-	fmt.Printf("%d\n", longestPalind(str))
+func longestPalindrome(str string) string {
+	if len(str) < 1 {
+		return ""
+	}
+	var start, end = 0, 0
+	byteStr := []byte(str)
+	for i := 0; i < len(str); i++ {
+		len1 := expandCenter(byteStr, i, i)
+		len2 := expandCenter(byteStr, i, i + 1)
+		tmp := max(len1, len2)
+		if tmp > end - start {
+			start = i - (tmp - 1) / 2
+			end = i + tmp / 2
+		}
+	}
+	return string(byteStr[start:end + 1])
+}
 
+func expandCenter(str []byte, left, right int) int {
+	l := left
+	r := right
+	for l >= 0 && r < len(str) && str[l] == str[r] {
+		l--
+		r++
+	}
+	return r - l - 1
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	var str = "ababa"
+	fmt.Printf("%d\n", longestPalind(str))
+	fmt.Printf("%s", longestPalindrome(str))
 
 	var ch = make(chan int, 1000)
 	go func() {
