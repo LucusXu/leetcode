98bc536408c13fd5d453cb4f8fcfc2958e0169fe
徐立秋 <xuliqiu@dongqiudi.com>
2020-05-18 17:30:07 +0800

    add huiwen

--- ks.go
+++ ks.go
@@ -4,9 +4,32 @@ import (
 	"fmt"
 )
 
+func longestPalind(str string) int {
+	var cache = make(map[byte]int, 0)
+	bstr := []byte(str)
+	for _, v := range bstr {
+		if _, ok := cache[v]; !ok {
+			cache[v] = 1
+		} else {
+			cache[v]++
+		}
+	}
+	var res = 0
+	for _, value := range cache {
+		res = res + value / 2 * 2
+		if res % 2 == 0 && value % 2 == 1 {
+			res++
+		}
+	}
+	return res
+}
 
 
 func main() {
+	var str = "abccccdd"
+	fmt.Printf("%d\n", longestPalind(str))
+
+
 	var ch = make(chan int, 1000)
 	go func() {
 		for i := 0; i < 1000; i++ {
